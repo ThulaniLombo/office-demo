fd5df0dd3944d8881601f65c18f561ac7772a458
thulanilombo7 <thulanilombo7@gmail.com>
2021-09-22 16:54:54 +0200

    environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyCxmdZAFdWbIb0sJ3WqUpAfhPhYqiRChJU",
+    authDomain: "office-man-app.firebaseapp.com",
+    projectId: "office-man-app",
+    storageBucket: "office-man-app.appspot.com",
+    messagingSenderId: "206539734239",
+    appId: "1:206539734239:web:d99482d6be0bc74ea13d6a",
+    measurementId: "G-G512VPE7N6"
+  }
 };
